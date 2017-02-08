51612779e3fac7aa2fce58afad321fdfe386ce0e
MarLeo <martin.aurele12@gmail.com>
2017-02-08 23:15:07 +0100

    first scala class...

---
+++ src/main/scala-2.11/WordCount.scala
@@ -0,0 +1,32 @@
+/**
+  * Created by marti on 08/02/2017.
+  */
+
+import org.apache.spark.{SparkConf, SparkContext}
+// for spark context and conf
+
+
+object WordCount {
+
+  def main(args: Array[String]): Unit = {
+
+    val conf = new SparkConf() // Defining spark configuration object
+      .setMaster("local[*]") // Setting spark as local Master
+      .setAppName("word count") // Spark Application name
+      .set("spark.executor.memory", "2g") // Setting spak execution memory size
+
+
+    val sc = new SparkContext(conf) // Actual starting execution point
+
+    val lines = sc.parallelize(Seq("This is first line", "This is second line", "This is third line"))
+    // Defining parallelize action on spark context
+
+    val counts = lines.flatMap(line => line.split(" ")) // flatmap operation
+      .map(word => (word, 1))
+      .reduceByKey(_ + _)
+    counts.foreach(println) // Count word and display
+
+  }
+
+
+}
