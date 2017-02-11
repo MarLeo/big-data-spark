2177d060948d5d51976634edfede856ac0b495c7
MarLeo <martin.aurele12@gmail.com>
2017-02-11 19:52:57 +0100

    add  dictionnary

--- src/main/scala-2.11/PairsWord.scala
+++ src/main/scala-2.11/PairsWord.scala
@@ -26,12 +26,15 @@ object PairsWord {
 
     val data = sc.textFile(inputPath.toString)
 
-    val pairs = data.flatMap{ line => line.split(",")
-                                          .combinations(2)
-                                          .toSeq
-                                          .map{ case list => list(0) -> list(1) } }
+    val pairs = data.flatMap{ line => line.split(",") // "word1,word2,word3" => Array(word1,word2,word3)
+                                          .combinations(2) // Iterator
+                                          .toSeq           // Array(Array(word1,word2), Array(word1,word3), Array(word2,word3))
+                                          .map{ case list => list(0) -> list(1) } } // Array((word1,word2),(word1,word3),(word2,word3))
 
-    val result = pairs.map( item => item -> 1).reduceByKey(_+_)
+    // Array((word1,word2),(word1,word3),(word2,word3),...)
+
+    val result = pairs.map( item => item -> 1) // Array(((word1,word2),1) ,((word1,word3),1),((word2,word3),1))
+                      .reduceByKey(_+_)   // Array(((word1,word2),1) ,((word1,word3),1),((word2,word3),1),...)
 
     //val result = data.map(line => line.split(",")).flatMap(value => value.sliding(2)).map(tuple => (tuple(0), tuple(1)) -> 1).reduceByKey(_+_)
 
